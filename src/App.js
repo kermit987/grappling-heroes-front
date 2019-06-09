781c33b26ef6a76fdef516fa27ac81d761f3564e
steven <steven.loofat@yahoo.fr>
2019-06-09 21:31:37 +0800

    [+] implementing the authentication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import Card from './Component/Card/Card.jsx'
+import Card from './Component/Card/Card.jsx';
+import Private from './Component/Private.jsx';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div>
-      <Card />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/login' component={Card} />
+            {/* <PrivateRoute exact path='/private' component={Private} /> */}
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -3,66 +3,74 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import Login from 'Component/Login/Login.js'
-import Password from 'Component/Password/Password.js'
-import Button from 'Component/Button/Button.js'
-import axios from 'axios'
+import Login from 'Component/Login/Login.js';
+import Password from 'Component/Password/Password.js';
+import Button from 'Component/Button/Button.js';
+import axios from 'axios';
 import 'css/cardLoginPassword.css';
-import 'css/signIn.css'
+import 'css/signIn.css';
 
 const styles = {
   card: {
-    maxWidth: 345,
+    maxWidth: 345
   },
   media: {
-    height: 140,
-  },
+    height: 140
+  }
 };
 
 class MediaCard extends React.Component {
   state = {
-    username: "",
-    password: ""
-  }
+    username: '',
+    password: ''
+  };
 
-  handleUsernameChange = (e) => {
-    this.setState({ username: e.target.value })
-  }
+  handleUsernameChange = e => {
+    this.setState({ username: e.target.value });
+  };
 
-  handlePasswordChange = (e) => {
-    this.setState({ password: e.target.value })
-  }
+  handlePasswordChange = e => {
+    this.setState({ password: e.target.value });
+  };
 
   submitButton = async () => {
-
     const user = {
       username: this.state.username,
       password: this.state.password
-    }
+    };
 
-    axios.post('http://localhost:8000/login', user).then(res => {
-      console.log('axios return value ', res)
-    })
+    console.log('in the submitButton ');
 
-    console.log('in the submitButton ')
-  }
+    const result = axios.post('http://localhost:3000/login', user, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    console.log('value of result ', result);
+  };
 
   render() {
     return (
       <Card>
-        <CardContent className="cardLoginPassword" >
-          <h1 className="signIn">Sign In</h1>
-          <Login value={this.state.username} onChange={this.handleUsernameChange} />
-          <Password value={this.state.password} onChange={this.handlePasswordChange} />
+        <CardContent className='cardLoginPassword'>
+          <h1 className='signIn'>Sign In</h1>
+          <Login
+            value={this.state.username}
+            onChange={this.handleUsernameChange}
+          />
+          <Password
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+          />
           <Button onClick={this.submitButton} />
         </CardContent>
       </Card>
-    )
+    );
   }
 }
 
 MediaCard.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(MediaCard);
